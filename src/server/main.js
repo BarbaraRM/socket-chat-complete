0111298fe364b099c07676ffc7ddd3fe4d8d3653
BarbaraRM <70588575+BarbaraRM@users.noreply.github.com>
2020-10-30 15:43:19 -0500

    Update main.js

--- src/server/main.js
+++ src/server/main.js
@@ -7,7 +7,7 @@ const app = express()
 const server = http.createServer(app)
 const io = socket(server)
 var port = process.env.PROT || 3000
-//app.set('port', process.env.PROT || 3000)
+//app.set('port', process.env.PORT || 3000)
 app.use(express.static(path.join(__dirname, '../public')))
 
 require('./socket')(io)
